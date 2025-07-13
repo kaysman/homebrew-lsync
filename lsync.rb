8d01197e077c7594f4646ed2a95ff722fac3e467
kaysman <ikays.holding.com>
2025-07-13 16:45:13 +0500

    upgrade version to 0.1.1

--- lsync.rb
+++ lsync.rb
@@ -1,10 +1,13 @@
 class Lsync < Formula
   desc "Localyze Sync CLI for syncing translations from Google Sheets"
-  homepage "https://github.com/kaysman/lsync"
-  version "0.1.0"
-  url "https://github.com/kaysman/lsync/releases/download/v0.1.1/lsync_cli_macos"
-  sha256 "5fa4694ca7eb7b05194497ab97351b69e52901f0c2990a704ddce009caa853d9"
   license "MIT"
+  homepage "https://github.com/kaysman/lsync"
+  # required
+  version "0.1.1"
+  # required
+  url "https://github.com/kaysman/lsync/releases/download/v0.1.1/lsync_cli_macos"
+  # required
+  sha256 "5fa4694ca7eb7b05194497ab97351b69e52901f0c2990a704ddce009caa853d9"
 
   def install
     bin.install "lsync_cli_macos" => "lsync"
