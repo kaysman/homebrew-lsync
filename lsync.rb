9d8f7b7071c8982bc7b7a7f2b472d2dc2b02116b
kaysman <ikays.holding.com>
2025-07-13 16:39:02 +0500

    Tap v0.1.1

--- lsync.rb
+++ lsync.rb
@@ -2,8 +2,8 @@ class Lsync < Formula
   desc "Localyze Sync CLI for syncing translations from Google Sheets"
   homepage "https://github.com/kaysman/lsync"
   version "0.1.0"
-  url "https://github.com/kaysman/lsync/releases/download/v0.1.0/lsync_cli_macos"
-  sha256 "563ae62967c7d986acfb9f425286df97afa95a8ed7a846a0fb2d87432e3c4610"
+  url "https://github.com/kaysman/lsync/releases/download/v0.1.1/lsync_cli_macos"
+  sha256 "5fa4694ca7eb7b05194497ab97351b69e52901f0c2990a704ddce009caa853d9"
   license "MIT"
 
   def install
